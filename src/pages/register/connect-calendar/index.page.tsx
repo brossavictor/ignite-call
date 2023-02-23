f73297a5377460015ba2cedfbbc5465e0897f4f3
Victor Brossa <brossajoao@gmail.com>
2023-02-23 17:49:38 -0500

    step 2 calendar connection done

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,63 @@
+import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
+import { AuthError, ConnectItem, ConnextBox } from "./styles";
+import { Container, Header } from "../styles";
+import { ArrowRight, Check } from "phosphor-react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+//import { api } from "@/src/lib/axios";
+
+export default function Register() {
+	const session = useSession();
+	const router = useRouter();
+
+	const hasAuthError = !!router.query.error;
+	const isSignedIn = session.status === "authenticated";
+
+	async function handleConnectCalendar() {
+		await signIn("google");
+	}
+
+	return (
+		<Container>
+			<Header>
+				<Heading as="strong">Connect yourself!</Heading>
+				<Text>
+					Connect your calendar in order to automatically update your schedule .
+				</Text>
+				<MultiStep size={4} currentStep={2} />
+			</Header>
+
+			<ConnextBox>
+				<ConnectItem>
+					<Text>Google Calendar</Text>
+					{isSignedIn ? (
+						<Button size="sm" disabled>
+							Connected <Check />
+						</Button>
+					) : (
+						<Button
+							variant="secondary"
+							size="md"
+							onClick={handleConnectCalendar}
+						>
+							Connect
+							<ArrowRight />
+						</Button>
+					)}
+				</ConnectItem>
+
+				{hasAuthError && (
+					<AuthError size="sm">
+						Error connecting with Google. Check if you have given permission to
+						have access to Google Calendar.
+					</AuthError>
+				)}
+
+				<Button type="submit" disabled={!isSignedIn}>
+					Next step
+					<ArrowRight />
+				</Button>
+			</ConnextBox>
+		</Container>
+	);
+}
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,8 +1,61 @@
+import { api } from "@/src/lib/axios";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
+import { AxiosError } from "axios";
+import { useRouter } from "next/router";
 import { ArrowRight } from "phosphor-react";
-import { Container, Form, Header } from "./styles";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { string, z } from "zod";
+import { Container, Form, FormError, Header } from "./styles";
 
-export default function Regiser() {
+const registerFormSchema = z.object({
+	username: z
+		.string()
+		.min(3, { message: "The username has to have three or more characters." })
+		.regex(/^([a-z\\-\\_]+$)/i, {
+			message: "Only letters, numbers, hyphens and underscores allowed.",
+		})
+		.transform((username) => username.toLowerCase()),
+	name: z
+		.string()
+		.min(3, { message: "The name has to have three or more characters." }),
+});
+
+type RegisterFormData = z.infer<typeof registerFormSchema>;
+
+export default function Register() {
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		formState: { errors, isSubmitting },
+	} = useForm<RegisterFormData>({
+		resolver: zodResolver(registerFormSchema),
+	});
+
+	const router = useRouter();
+
+	useEffect(() => {
+		if (router.query.username) {
+			setValue("username", String(router.query.username));
+		}
+	}, [router.query?.username, setValue]);
+
+	async function handleRegister(data: RegisterFormData) {
+		try {
+			await api.post("/users", { name: data.name, username: data.username });
+			await router.push("/register/connect-calendar");
+		} catch (err) {
+			if (err instanceof AxiosError && err?.response?.data?.message) {
+				alert(err.response.data.message);
+				return;
+			}
+
+			console.log(err);
+		}
+	}
+
 	return (
 		<Container>
 			<Header>
@@ -13,25 +66,30 @@ export default function Regiser() {
 				</Text>
 				<MultiStep size={4} currentStep={1} />
 			</Header>
-			<Form as="form">
+
+			<Form as="form" onSubmit={handleSubmit(handleRegister)}>
 				<label>
-					<Text as="span" size="sm">
-						Username
-					</Text>
+					<Text size="sm">Username</Text>
 					<TextInput
 						prefix="ignite.com/"
 						placeholder="your-username"
+						{...register("username")}
 					></TextInput>
+
+					{errors.name && (
+						<FormError size="sm">{errors.username?.message}</FormError>
+					)}
 				</label>
 
 				<label>
-					<Text as="span" size="sm">
-						Full name
-					</Text>
-					<TextInput placeholder="Your name"></TextInput>
+					<Text size="sm">Full name</Text>
+					<TextInput placeholder="Your name" {...register("name")}></TextInput>
+					{errors.name && (
+						<FormError size="sm">{errors.name?.message}</FormError>
+					)}
 				</label>
 
-				<Button type="submit">
+				<Button type="submit" disabled={isSubmitting}>
 					Next step <ArrowRight />
 				</Button>
 			</Form>
